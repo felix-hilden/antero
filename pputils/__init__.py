e97a145e233ba25f6cee9aabfccfe0b74e10a84c
Felix Hildén <felix.hilden@gmail.com>
2019-03-09 11:41:59 +0200

    Print before and after timer

--- pputils/__init__.py
+++ pputils/__init__.py
@@ -3,7 +3,15 @@ from time import perf_counter
 
 
 @contextmanager
-def timer(msg: str = 'Timer: %.3f'):
+def timer(start: str = None, end: str = None):
+    if start is not None:
+        print(start)
+
+    if start is None and end is None:
+        end = 'Timer: %.3f'
+    elif start is not None and end is None:
+        end = start + ': %.3f'
+
     t = perf_counter()
     yield t, perf_counter
-    print(msg % (perf_counter() - t))
+    print(end % (perf_counter() - t))
