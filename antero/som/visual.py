bdf9a9c639eb98ccf3f5bfd62f54ec1bd6fb1bae
Felix Hildén <felix.hilden@gmail.com>
2019-04-13 00:14:33 +0300

    Accept pd.Categoricals and use continuous colormap for ordinal data

--- antero/som/visual.py
+++ antero/som/visual.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pandas as pd
 import seaborn as sns
 
 import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib.gridspec import GridSpec
+from scipy.cluster.hierarchy import linkage, dendrogram as _dendrogram
 
 from antero.som import _BaseSOM
 from antero.som.measures import umatrix as _umatrix
@@ -32,28 +34,36 @@ def heatmap(som: _BaseSOM, x: np.ndarray, y: np.ndarray = None) -> None:
             plt.pause(0.1)
 
 
-def labelmap(som: _BaseSOM, x: np.ndarray, y: np.ndarray) -> None:
+def labelmap(som: _BaseSOM, x: np.ndarray, y, ordinal: bool = False) -> None:
     """
     Produce a label map assigning a class to each node based on the most frequent class.
 
     :param som: self-organising map instance
     :param x: data samples
-    :param y: true numerical labels
+    :param y: labels, either pd.Series of pd.Categorical or np.ndarray of numerical labels
+    :param ordinal: indicate ordinal data to choose a continuous color map
     :return: None
     """
+    if isinstance(y.dtype, pd.CategoricalDtype):
+        names = y.cat.categories.values
+        y = y.cat.codes.values
+        n_labs = y.max() + 1
+    else:
+        n_labs = y.max() + 1
+        names = np.array([str(i) for i in range(n_labs)])
+
     labels = som.labelmap(x, y)
-    n_labs = y.max() + 1
 
     y_ticks = [str(i) for i in range(labels.shape[0])]
     x_ticks = [str(i) for i in range(labels.shape[1])]
 
-    names = np.array([str(i) for i in range(n_labs)])
     norm = matplotlib.colors.BoundaryNorm(np.linspace(-0.5, n_labs-0.5, n_labs+1), n_labs)
     fmt = matplotlib.ticker.FuncFormatter(lambda z, pos: names[norm(z)])
 
     plt.figure()
+    cmap = 'tab20' if not ordinal else 'copper'
     im, _ = _heatmap(
-        labels, y_ticks, x_ticks, cmap=plt.get_cmap('tab20', n_labs), norm=norm,
+        labels, y_ticks, x_ticks, cmap=plt.get_cmap(cmap, n_labs), norm=norm,
         cbar_kw=dict(ticks=np.arange(n_labs), format=fmt),
         cbarlabel='Class label'
     )
