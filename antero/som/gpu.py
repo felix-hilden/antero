3dfd91355c891ba766b41092ffa4fe4028b7b6d2
Felix Hildén <felix.hilden@gmail.com>
2020-11-02 16:41:40 +0200

    Convert to TF 2

--- antero/som/gpu.py
+++ antero/som/gpu.py
@@ -5,38 +5,35 @@ from tqdm import tqdm
 from antero.som import _BaseSOM
 
 
-def _place_learning_rate(
-        epoch: tf.placeholder,
+def _learning_rate(
+        epoch,
         max_epochs: int,
         lr_decay: float = 1
 ):
-    with tf.name_scope('learning_rate'):
-        return tf.exp(-4 / max_epochs * lr_decay * epoch )
+    return tf.cast(tf.exp(-4 / max_epochs * lr_decay * epoch), tf.float64)
 
 
-def _place_neighbourhood(
+def _neighbourhood(
         shape: tuple,
-        r: tf.placeholder,
-        epoch: tf.placeholder,
+        r,
+        epoch,
         max_epochs: int,
         nbh_width: float = 1,
         nbh_decay: float = 1
 ):
     c = 4 / max(shape) / nbh_width
-    with tf.name_scope('neighbourhood'):
-        return tf.exp(
-            - (c * r / (1 - epoch / max_epochs) ** nbh_decay) ** 2
-        )
+    return tf.exp(
+        - (c * r / (1 - epoch / max_epochs) ** nbh_decay) ** 2
+    )
 
 
 class SelfOrganisingMap(_BaseSOM):
-    @property
-    def initialiser(self):
+    def initialiser(self, shape):
         if self._weights is None:
             if self._initialiser == 'uniform':
-                return tf.random_uniform_initializer
+                return tf.random_uniform_initializer()(shape=shape)
             elif self._initialiser == 'normal':
-                return tf.random_normal_initializer
+                return tf.random_normal_initializer()(shape=shape)
         else:
             return tf.convert_to_tensor(self._weights)
 
@@ -49,7 +46,9 @@ class SelfOrganisingMap(_BaseSOM):
             verbose: bool = False
     ) -> None:
         """
-        Create training graph and train SOM.
+        Train SOM.
+
+        Note that this method is unoptimised, so it is slow compared to CPU.
 
         :param x: training data
         :param epochs: number of epochs to train
@@ -58,75 +57,54 @@ class SelfOrganisingMap(_BaseSOM):
         :param verbose: display a progress bar
         :return: None
         """
-        graph = tf.Graph()
-        sess = tf.Session(graph=graph)
-
         x = x.astype(np.float64)
 
         batches = x.shape[0] // batch_size
         if x.shape[0] % batch_size != 0:
             raise ValueError('Bad batch_size, last batch would be incomplete!')
 
-        # Construct graph
-        with graph.as_default():
-            indices = tf.convert_to_tensor(np.expand_dims(
-                np.indices(self.shape, dtype=np.float64), axis=-1
-            ))
-            weights = tf.get_variable(
-                'weights', (*self.shape, 1, self.features), initializer=self.initialiser, dtype=tf.float64
-            )
-            curr_epoch = tf.placeholder(dtype=tf.int64, shape=())
+        indices = tf.convert_to_tensor(np.expand_dims(
+            np.indices(self.shape, dtype=np.float64), axis=-1
+        ), dtype=tf.float64)
+        weights = tf.cast(tf.Variable(
+            self.initialiser(shape=(*self.shape, 1, self.features))
+        ), tf.float64)
 
-            with tf.name_scope('data'):
-                data = tf.data.Dataset.from_tensor_slices(x)
-                if shuffle:
-                    data = data.shuffle(buffer_size=10000)
-                data = data.repeat(epochs)
-                data = data.batch(batch_size, drop_remainder=True)
-                data = data.make_one_shot_iterator().get_next()
-
-            def train_loop(w):
-                with tf.name_scope('winner'):
-                    diff = w - data
-                    dist = tf.reduce_sum(diff ** 2, axis=-1, keepdims=True)
-                    w_ix = tf.argmin(tf.reshape(dist, (self.n_nodes, data.shape[0])), axis=0)
-                    winner_op = tf.convert_to_tensor(tf.unravel_index(w_ix, self.shape))
-
-                with tf.name_scope('update'):
-                    idx_diff = indices - tf.reshape(tf.cast(
-                        winner_op, dtype=tf.float64
-                    ), shape=self._neighbour_shape)
-                    idx_dist = tf.norm(idx_diff, axis=0)
-
-                    l_rate = _place_learning_rate(
-                        curr_epoch,
-                        self.max_epochs,
-                        self._learning_rate_decay
-                    )
-                    n_hood = _place_neighbourhood(
-                        self.shape,
-                        idx_dist,
-                        curr_epoch,
-                        self.max_epochs,
-                        self._neighbourhood_width,
-                        self._neighbourhood_decay
-                    )
-
-                    update = diff * l_rate * tf.expand_dims(n_hood, axis=-1)
-                    return w - self._initial_lr * tf.reduce_mean(update, axis=-2, keepdims=True)
-
-            n_weights = tf.while_loop(lambda _: True, train_loop, (weights,), maximum_iterations=batches)
-            update_op = weights.assign(n_weights)
-
-            init = tf.global_variables_initializer()
-
-        sess.run(init)
+        data = tf.data.Dataset.from_tensor_slices(x)
+        if shuffle:
+            data = data.shuffle(buffer_size=x.shape[0])
+        data = data.repeat(epochs)
+        data = data.batch(batch_size, drop_remainder=True)
+        data_it = iter(data)
 
         for i in tqdm(range(epochs)) if verbose else range(epochs):
-            sess.run(update_op, feed_dict={
-                curr_epoch: self.epochs + i
-            })
+            curr_epoch = self.epochs + i
+            for _ in range(batches):
+                batch = next(data_it)
+                diff = weights - batch
+                dist = tf.reduce_sum(diff ** 2, axis=-1, keepdims=True)
+                winner_ix = tf.argmin(tf.reshape(dist, (self.n_nodes, batch.shape[0])), axis=0)
+                winner = tf.unravel_index(winner_ix, self.shape)
+                winner = tf.cast(tf.reshape(winner, shape=self._neighbour_shape), tf.float64)
+                idx_dist = tf.norm(indices - winner, axis=0)
 
-        self._weights = sess.run(weights)
+                l_rate = _learning_rate(
+                    curr_epoch,
+                    self.max_epochs,
+                    self._learning_rate_decay
+                )
+                n_hood = _neighbourhood(
+                    self.shape,
+                    idx_dist,
+                    curr_epoch,
+                    self.max_epochs,
+                    self._neighbourhood_width,
+                    self._neighbourhood_decay
+                )
+                update = diff * l_rate * tf.expand_dims(n_hood, axis=-1)
+                mean_update = tf.reduce_mean(update, axis=-2, keepdims=True)
+                weights = weights - self._initial_lr * mean_update
+
+        self._weights = weights.numpy()
         self._epochs += epochs
 
